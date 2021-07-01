7441d35153b174f95ab105361cd17178c5bb8121
fruity nik <nikandr33@inbox.ru>
2021-07-01 14:04:27 +0300

    add user change legal entity info

---
+++ upload/catalog/language/ru-ru/account/edit.php
@@ -0,0 +1,41 @@
+<?php
+// Heading
+$_['heading_title']      = 'Учетная запись';
+
+// Text
+$_['text_account']       = 'Личный Кабинет';
+$_['text_edit']          = 'Редактировать информацию';
+$_['text_your_details']  = 'Ваша учетная запись';
+$_['text_success']       = 'Ваша учетная запись была успешно обновлена!';
+$_['text_vat'] 		  		  	 = 'НДС';
+$_['text_legal_entity_form'] 	 = 'Форма юр. лица';
+$_['text_organization'] 		 = 'Организация';
+$_['text_index'] 		         = 'Индекс';
+$_['text_legal_address'] 		 = 'Юридический адрес';
+$_['text_mailing_address'] 		 = 'Почтовый адрес';
+$_['text_inn'] 		             = 'ИНН';
+$_['text_kpp'] 		             = 'КПП';
+$_['text_current_account_number']= 'Номер расчетного счета';
+$_['text_bank_name'] 	         = 'Наименование банка';
+$_['text_bic'] 	                 = 'БИК';
+$_['text_bank_correspondent_account']= 'Кор. счет банка';
+$_['text_head_fullname']         = 'ФИО руководителя';
+$_['text_business_registration'] = 'Регистрация в "Netto net для бизнеса"';
+$_['text_legal_entities_info']   = 'Юридическая информация плательщика';
+
+
+// Entry
+$_['entry_firstname']    = 'Имя, Отчество';
+$_['entry_lastname']     = 'Фамилия';
+$_['entry_email']        = 'E-Mail';
+$_['entry_telephone']    = 'Телефон';
+
+
+// Error
+$_['error_exists']       = 'Данный E-Mail уже зарегистрирован!';
+$_['error_firstname']    = 'Имя должно быть от 1 до 32 символов!';
+$_['error_lastname']     = 'Фамилия должна быть от 1 до 32 символов!';
+$_['error_email']        = 'E-Mail адрес введен неверно!';
+$_['error_telephone']    = 'Номер телефона должен быть от 3 до 32 символов!';
+$_['error_custom_field'] = '%s необходим!';
+
